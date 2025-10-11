1a212e5da9425aaf1370ea91f91c465375bb46be
Edwin32322 <edwincastellanos150@gmail.com>
2025-10-11 17:46:46 -0500

    Add config in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import Express from "express";
 import { configDotenv } from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
 
-configDotenv()
+configDotenv();
 
 const app = Express();
+
+app.use('/api/data', userRoutes);
+
+app.use(Express.json());
 
 const PORT = process.env.PORT;
 
