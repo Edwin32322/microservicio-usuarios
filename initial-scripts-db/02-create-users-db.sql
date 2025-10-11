eb85107ce01d7b904db391643808a0764a07081c
Edwin32322 <edwincastellanos150@gmail.com>
2025-10-11 17:44:20 -0500

    Create Docker Compose
    Se crea un docker compose para levantar un contenedor de PostgreSQL, con persistencia de datos y para inicializar con scripts SQL

---
+++ initial-scripts-db/02-create-users-db.sql
@@ -0,0 +1,4 @@
+INSERT INTO usuarios (nombre, correo, edad) VALUES 
+('Edwin Goaly', 'edwincastellanos150@gmail.com', 20),
+('Leonel Messi', 'leoNessi@gmail.com', 38),
+('Tom Aspinall', 'aspinallbjj@gmail.com', 32);
