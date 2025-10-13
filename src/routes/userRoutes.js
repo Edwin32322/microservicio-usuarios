0478deb84b550dfc1657ae8b0cf146a2fc8ca96e
Edwin32322 <edwincastellanos150@gmail.com>
2025-10-13 12:56:13 -0500

    add user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,6 +3,9 @@ import { userController } from "../controllers/userController.js";
 
 const userRoutes = Router();
 
-userRoutes.get("/", userController.getAllUser);
+userRoutes.get("/", userController.getAllUsers);
+userRoutes.post("/", userController.createUser)
+userRoutes.post("/delete-users", userController.deleteUsers)
+userRoutes.put("/:userId", userController.updateUser)
 
 export default userRoutes;
