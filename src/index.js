7b1e21d4574afffc98588215638344f9d1f2b855
Edwin32322 <edwincastellanos150@gmail.com>
2025-10-13 12:53:22 -0500

    add cors configuration and use user routes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import Express from "express";
 import { configDotenv } from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
 
 configDotenv();
 
 const app = Express();
 
-app.use('/api/data', userRoutes);
+app.use(cors({
+    origin: process.env.ORIGIN_FRONTEND,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(Express.json());
+
+app.use('/api/data', userRoutes);
 
 const PORT = process.env.PORT;
 
